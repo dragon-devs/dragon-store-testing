5c1897c1a8c0aeca6aebfa131d21be11a3c4edf2
Dragon Robust <dragonfourtyseven@gmail.com>
2022-12-26 18:19:59 +0500

    first

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -10,21 +10,19 @@ SECRET_KEY = 'django-insecure-obkpqm_zxou#4l0v8)k!ecd0e$2o56vep(rqyw+5qj*#=5#&ju
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefrontwsl3',
+        'NAME': 'storefront3',
         'USER': 'root',
-        'PASSWORD': 'Brucelee28',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    
+        'PASSWORD': 'MyPassword',
+        'HOST': 'mysql',    
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "TIMEOUT": 10 * 60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
@@ -32,8 +30,12 @@ CACHES = {
     }
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = 'from@dragondevs.com'
+
+DEBUG_TOOBAR_CONFIG = {
+    'SHOW_TOOBAR_CALLBACK': lambda request:True,
+}
